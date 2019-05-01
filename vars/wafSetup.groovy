2dda84a1cab44447be9071a621cb2ede5d1bee20
Yannik Stradmann <yannik@stradmann.name>
2019-05-01 14:27:28 +0200

    Allow wafSetup calls without additional stages
    Change-Id: Ied2e7c3fe671af70cd3bed96f93eca9ba3f67265

--- vars/wafSetup.groovy
+++ vars/wafSetup.groovy
@@ -7,10 +7,24 @@
  *                    <li><b>projects</b> (mandatory): List of projects to be built
  *                    <li><b>wafOptions</b> (optional): Options passed to <code>withWaf</code>
  *                    <li><b>setupOptions</b> (optional): Options passed to the <code>waf setup</code> call.
- *  *                                                    Defaults to <code>"--clone-depth=1"</code>
+ *                                                        Defaults to <code>"--clone-depth=1"</code>
+ *                    <li><b>noExtraStage</b> (optional): Don't wrap the waf setup in a dedicated stage.
+ *                                                        Defaults to <code>false</code>.
  *                </ul>
  */
 def call(Map<String, Object> options = [:]) {
+	boolean noExtraStage = options.get("noExtraStage", false)
+
+	if (noExtraStage) {
+		return impl(options)
+	}
+
+	stage('Waf Setup') {
+		return impl(options)
+	}
+}
+
+def impl(Map<String, Object> options = [:]) {
 	if (!options.containsKey("projects")) {
 		throw new IllegalArgumentException("No projects given to set up.")
 	}
@@ -26,15 +40,13 @@ def call(Map<String, Object> options = [:]) {
 	String projectCommand = projects.join(" --project ").trim()
 
 	withWaf(options.get("wafOptions", [:])) {
-		stage('Waf Setup') {
-			runOnSlave(label: "frontend") {
-				if (env.GERRIT_CHANGE_NUMBER) {
-					jesh("waf setup ${projectCommand} ${setupOptions} " +
-					     "--gerrit-changes=${GERRIT_CHANGE_NUMBER} " +
-					     "--gerrit-url=ssh://${getGerritUsername()}@${GERRIT_HOST}:${GERRIT_PORT}")
-				} else {
-					jesh("waf setup ${projectCommand} ${setupOptions}")
-				}
+		runOnSlave(label: "frontend") {
+			if (env.GERRIT_CHANGE_NUMBER) {
+				jesh("waf setup ${projectCommand} ${setupOptions} " +
+				     "--gerrit-changes=${GERRIT_CHANGE_NUMBER} " +
+				     "--gerrit-url=ssh://${getGerritUsername()}@${GERRIT_HOST}:${GERRIT_PORT}")
+			} else {
+				jesh("waf setup ${projectCommand} ${setupOptions}")
 			}
 		}
 	}
