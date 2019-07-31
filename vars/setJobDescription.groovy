a291ef1178da7cb80e5d5fa4b02da850ecd50e08
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2019-08-01 01:03:35 +0200

    only change job description if different from current; includes lock
    Change-Id: I132413e54985c82bf5779a60f1a9f0c3c86dd082

--- vars/setJobDescription.groovy
+++ vars/setJobDescription.groovy
@@ -2,5 +2,12 @@
  * Set the description of the job (not current build) from within a Jenkinsfile.
  */
 def call(String description) {
-	currentBuild.rawBuild.project.description = description
+	// with check, changing description not atomic. note that the ressoure
+	// JENLIB_JOB_CONFIGURATION_UPDATE must be a registered lockable ressource
+	lock("JENLIB_JOB_CONFIGURATION_UPDATE"){
+		// only change if not already set. this helps keep the number of config changes small
+		if (currentBuild.rawBuild.project.description != description) {
+			currentBuild.rawBuild.project.description = description
+		}
+	}
 }
