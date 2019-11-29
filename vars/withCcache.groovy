bc847bd25b678c35dbdd9bb38e1341ea382688a7
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2019-11-29 13:15:18 +0100

    Make withCcache useable outside node allocations
    Change-Id: Idae3ebb280c5f880e563c32c63737f9b2ba5f5c0

--- vars/withCcache.groovy
+++ vars/withCcache.groovy
@@ -1,3 +1,5 @@
+import org.electronicvisions.jenlib.SharedWorkspace
+
 /**
  * Run a section of code using ccache.
  *
@@ -6,7 +8,7 @@
  *                    <li><b>ccachePath</b> (optional), defaults to <code>/usr/lib/ccache</code></li>
  *                    <li><b>ccacheDir</b> (optional), defaults to <code>/scratch/jenkins/ccache/$JOB_BASE_NAME</code></li>
  *                    <li><b>ccacheTmpdir</b> (optional), defaults to <code>/scratch/jenkins/ccache/$JOB_BASE_NAME</code></li>
- *                    <li><b>ccacheBasedir</b> (optional), defaults to <code>$WORKSPACE</code></li>
+ *                    <li><b>ccacheBasedir</b> (optional), defaults to <code>$WORKSPACE</code> if set, else to the shared workspace.</li>
  *                    <li><b>ccacheMaxsize</b> (optional), defaults to <code>25.0G</code></li>
  *                    <li><b>ccacheNoHashDir</b> (optional), defaults to <code>true</code>. Has to be boolean.</li>
  *                </ul>
@@ -15,10 +17,23 @@
 
 void call(Map options = [:], Closure content) {
 
+	/*
+	 * Heuristic for finding the default ccache base dir relative to which all paths are modified.
+	 * If this code runs within a node allocation (and $WORKSPACE is therefore set), the current workspace
+	 * is used.
+	 * If we run outside a node allocation, the {@link SharedWorkspace} is used.
+	 * */
+	String defaultBasedir
+	try {
+		defaultBasedir = $WORKSPACE
+	} catch (MissingPropertyException ignored) {
+		defaultBasedir = SharedWorkspace.getWorkspace(this)
+	}
+
 	ccachePath = options.get("ccachePath", "/usr/lib/ccache")
 	ccacheDir = options.get("ccacheDir", "/scratch/jenkins/ccache/$JOB_BASE_NAME")
 	ccacheTmpdir = options.get("ccacheTmpdir", "/scratch/jenkins/$JOB_BASE_NAME")
-	ccacheBasedir = options.get("ccacheBasedir", "$WORKSPACE")
+	ccacheBasedir = options.get("ccacheBasedir", defaultBasedir)
 	ccacheMaxsize = options.get("ccacheMaxsize", "25.0G")
 	ccacheNoHashDir = options.get("ccacheNoHashDir", true)
 
