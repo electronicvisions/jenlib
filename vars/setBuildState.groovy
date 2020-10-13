2677b63ba5b821f9b2aba9aa987efb04c2e806ab
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2020-10-13 14:29:23 +0200

    Make 'reason' argument in setBuildState mandatory (issue #3573)
    Change-Id: I4906e8fb0932e7de939a4e8099e345d626d89295

--- vars/setBuildState.groovy
+++ vars/setBuildState.groovy
@@ -25,14 +25,3 @@ def call(Map<String, Object> options) {
 	echo("[jenlib] Setting build state to '${state}'. Reason: ${reason}")
 	currentBuild.rawBuild.@result = Result.metaClass.getAttribute(Result, state)
 }
-
-/**
- * This method allows to set the build result of the current job in an arbitrary way.
- * The default setter only allows results to get worse.
- *
- * @see <a href="https://github.com/jenkinsci/jenkins/blob/578d6ba/core/src/main/java/hudson/model/Run.java">setResult()</a>
- * @param state Result to be set, one of ["NOT_BUILT", "UNSTABLE", "SUCCESS", "FAILURE", "ABORTED"]
- */
-def call(String state) {
-	currentBuild.rawBuild.@result = Result.metaClass.getAttribute(Result, state)
-}
