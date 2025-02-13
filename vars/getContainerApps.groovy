ead68fe4466d0582344fd2ae658c2160011eddfa
Eric Müller <mueller@kip.uni-heidelberg.de>
2025-02-13 10:57:25 +0100

    fix: support apps listing for modern apptainer
    Change-Id: Id3c8f1f146292ef69fefe5219d77a71558aae456

--- vars/getContainerApps.groovy
+++ vars/getContainerApps.groovy
@@ -7,7 +7,15 @@
 List<String> call(String containerPath) {
 	String containerImage = containerPath ?: getDefaultContainerPath()
 
-	return jesh(script: "singularity apps $containerImage", returnStdout: true).split("\n").collect { it.trim() }
+	// modern singularity versions (starting 3.4) and all apptainer versions do not support the apps subcommand anymore
+	// examples:
+	// apptainer version 1.3.6
+	// singularity version 3.1.0-rc2.37.ge3f4c52
+	if (jesh(script: "singularity --version", returnStdout: true).contains("apptainer")) {
+		return jesh(script: "singularity -s inspect --list-apps $containerImage", returnStdout: true).split("\n").collect { it.trim() }
+	} else {
+		return jesh(script: "singularity apps $containerImage", returnStdout: true).split("\n").collect { it.trim() }
+	}
 }
 
 List<String> call() {
